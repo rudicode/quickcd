b5478e9616ad93b957bc68fca7895cbb77bd8a18
Rudi Luzar <rudi@rudicode.com>
2015-07-17 14:40:51 -0400

    Started on the list functionality.

--- lib/quickcd/config_parser.rb
+++ lib/quickcd/config_parser.rb
@@ -1,4 +1,21 @@
 module Quickcd
   class ConfigParser
+
+  attr_reader :config_pairs
+
+    def initialize path
+      @config_pairs = {}
+      init_dummy_keys
+    end
+
+    def init_dummy_keys
+      @config_pairs = {
+        '1' => "~/test1",
+        '2' => "~/test2",
+        '3' => "~/test3",
+        '4' => "~/test4",
+        '5' => "~/test5"
+      }
+    end
   end
 end
--- lib/quickcd/qcd.rb
+++ lib/quickcd/qcd.rb
@@ -1,6 +1,17 @@
 module Quickcd
   class Qcd
     def initialize path
+      puts "Path: #{path}"
+      @config_parser = Quickcd::ConfigParser.new(path)
     end
+
+    def list
+      output = "List of saved directories.\n"
+      @config_parser.config_pairs.each do |key,value|
+        output << "#{key} = #{value}\n"
+      end
+      output
+    end
+
   end
 end
